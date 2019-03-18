c556f79293d585365dab400d7443837fdecc6828
MaxBrohman <maksimbrochman@gmail.com>
2019-03-18 23:31:06 +0300

    app-start added

---
+++ js/app-start.js
@@ -0,0 +1,85 @@
+//app-start.js
+import countTerms from './count-terms';
+import Accordion from './accordion';
+import DateInputs from './date-inputs';
+
+const isVisible = (elem) => {
+    const elemCoords = elem.getBoundingClientRect();
+    const topVisible = elemCoords.top > 0 && elemCoords.top < document.documentElement.clientHeight;
+    const bottomVisible = elemCoords.bottom > 0 && elemCoords.bottom < document.documentElement.clientHeight
+    return topVisible || bottomVisible;
+};
+const removeColors = (elem) => {
+    elem.style.backgroundColor = '';
+    elem.style.color = '';
+};
+const setColors = (elem, color, bgc) => {
+    elem.style.backgroundColor = bgc;
+    elem.style.color = color;
+}; 
+const getFormattedDate = () => {
+    const currentDate = new Date();
+    const HALF_YEAR_AGO = currentDate.getDate() - 180;
+    return new Intl.DateTimeFormat("ru").format(currentDate.setDate(HALF_YEAR_AGO));
+};
+export default class {
+    constructor(){
+        this.terms = document.querySelector('.app-interface__terms-container');
+        this.inputs = DateInputs.renderInitialInputs();
+        this.start = document.querySelector('.app-interface__app-start-button');
+        this.reset = document.querySelector('.app-interface__app-reset-button');
+        this.result = document.querySelector('.app-interface__app-result');
+        this.accordion = new Accordion(
+            document.querySelectorAll('.app-description__accordion-item'),
+            '.app-description__accordion-body'
+        );
+        this.showAccordion = this.showAccordion.bind(this);
+        this.showInputs = this.showInputs.bind(this);
+        this.startHandler = this.startHandler.bind(this);
+        this.resetHandler = this.resetHandler.bind(this);
+        this.resetHover = this.resetHover.bind(this);
+    }
+    startHandler(evt){
+        evt.preventDefault();
+        countTerms();
+    }
+    resetHandler(evt){
+        evt.preventDefault();
+        this.terms.reset();
+        this.terms.innerHTML = '';
+        this.terms.appendChild(this.inputs);
+        this.result.style.display = 'none';
+    }
+    resetHover(){
+        setColors(this.start, '#EC2127', '#fff');
+        setColors(this.reset, '#fff', '#EC2127');
+        this.reset.onmouseout = () => {
+            removeColors(this.start);
+            removeColors(this.reset);
+        }
+    }
+    showAccordion(){
+        const container = document.querySelector('.app-description__accordion');
+        if(isVisible(container)){
+            this.accordion.init();
+            window.removeEventListener('scroll', this.showAccordion);
+        }
+    }
+    showInputs(){
+        if(isVisible(this.terms)){
+            this.terms.appendChild(this.inputs);
+            window.removeEventListener('scroll', this.showInputs);
+            this.start.addEventListener('click', this.startHandler);
+            this.reset.addEventListener('click', this.resetHandler);
+        }
+    }
+    insertDate(date, elems){
+        elems.forEach(elem => elem.textContent = date);
+    }
+    init(){
+        window.addEventListener('scroll', this.showAccordion);
+        window.addEventListener('scroll', this.showInputs);
+        this.insertDate(getFormattedDate(), document.querySelectorAll('.date--counted'));
+        this.reset.addEventListener('mouseover', this.resetHover);
+    }
+};
--- js/index.js
+++ js/index.js
@@ -1,50 +1,5 @@
 import '../css/style.css';
-import Accordion from './accordion';
-import DateInputs from './date-inputs';
-import complexHover from './complex-hover';
-import countTerms from './count-terms';
-import getCountedDate from './get-counted-date';
+import AppStart from './app-start';
 
-const isVisible = (elem) => {
-    const elemCoords = elem.getBoundingClientRect();
-    const topVisible = elemCoords.top > 0 && elemCoords.top < document.documentElement.clientHeight;
-    const bottomVisible = elemCoords.bottom > 0 && elemCoords.bottom < document.documentElement.clientHeight
-    return topVisible || bottomVisible;
-};
-const accordionSection = document.querySelector('.app-description__accordion');
-const termsContainer = document.querySelector('.app-interface__terms-container');
-const startAccordion = () => {
-    if(isVisible(accordionSection)){
-        const accordion = new Accordion(
-            document.querySelectorAll('.app-description__accordion-item'),
-            '.app-description__accordion-body'
-        );
-        accordion.init();
-        window.removeEventListener('scroll', startAccordion);
-    }
-};
-const startTerms = () => {
-    if(isVisible(termsContainer)){
-        termsContainer.appendChild(DateInputs.renderInitialInputs());
-        const appResetButton = document.querySelector('.app-interface__app-reset-button');
-        appResetButton.addEventListener('click', (evt)=>{
-            evt.preventDefault();
-            termsContainer.reset();
-            termsContainer.innerHTML = "";
-            termsContainer.appendChild(DateInputs.renderInitialInputs());
-            document.querySelector('.app-interface__app-result').style.display = 'none';
-
-        });
-        const appStartButton = document.querySelector('.app-interface__app-start-button');
-        appStartButton.addEventListener('click', () => {
-            countTerms();
-        });
-        appResetButton.addEventListener('mouseover', ()=>{
-            complexHover(appResetButton, appStartButton, '#EC2127', '#fff');
-        });
-        window.removeEventListener('scroll', startTerms);
-    }
-};
-getCountedDate();
-window.addEventListener('scroll', startAccordion);
-window.addEventListener('scroll', startTerms);
+const app = new AppStart();
+app.init();
